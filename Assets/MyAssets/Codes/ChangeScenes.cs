a2a49d72a373ea866a26238a1f93403f1365e967
Hoa Pham <hoa.minh.pham88@gmail.com>
2024-07-14 23:21:41 +0300

    prototype assets added

--- Assets/MyAssets/Codes/ChangeScenes.cs
+++ Assets/MyAssets/Codes/ChangeScenes.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
